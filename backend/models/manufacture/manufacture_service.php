c001576f3c83ecdb8c6015706b6491eb759856ce
Ishani Pathinayake <ishanipathinayake@gmail.com>
2015-03-12 21:48:18 +0530

    Added functions for manufacture_serive.php

--- backend/models/manufacture/manufacture_service.php
+++ backend/models/manufacture/manufacture_service.php
@@ -6,5 +6,15 @@ class Manufacture_service extends CI_Model {
         parent::__construct();
         $this->load->model('manufacture/manufacture_model');
     }
+    
+    public function get_all_manufactures() {
+
+        $this->db->select('*');
+        $this->db->from('manufacture');
+        $this->db->where('is_deleted','0');
+        $this->db->order_by("added_date", "desc");
+        $query = $this->db->get();
+        return $query->result();
+    }
 
 }
