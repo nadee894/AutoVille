b75559ff28271cecaa7f7997885e06b9d5f25e5b
gayathma <gayathma3@gmail.com>
2015-03-12 23:30:54 +0530

    :accept:

--- backend/views/transmission/manage_transmission_view.php
+++ backend/views/transmission/manage_transmission_view.php
@@ -62,19 +62,20 @@
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                 <h4 class="modal-title">Add new transmission</h4>
-            </div>
-            <div class="modal-body">
-                <form id="add_transmission_form" name="add_transmission_form">
+            </div><form id="add_transmission_form" name="add_transmission_form">
+                <div class="modal-body">
+
                     <div class="form-group">
                         <label for="name">Title</label>
                         <input id="name" class="form-control" type="name" placeholder="Enter Title">
                     </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button data-dismiss="modal" class="btn btn-default" type="button">Close</button>
-                <button class="btn btn-success" type="button">Save changes</button>
-            </div>
+                </div>
+                <div class="modal-footer">
+                    <button data-dismiss="modal" class="btn btn-default" type="button">Close</button>
+                    <button class="btn btn-success" type="submit">Save changes</button>
+                </div>
+            </form>
+
         </div>
     </div>
 </div>
--- backend_resources/custom_js/custom_gaya.js
+++ backend_resources/custom_js/custom_gaya.js
@@ -3,15 +3,29 @@ $(document).ready(function() {
     $('#transmission_table').dataTable({
         "aaSorting": [[4, "desc"]]
     });
-    
-    
+
+
     $("#add_transmission_form").validate({
-            rules: {
-                name: "required"
-            },
-            messages: {
-                name: "Please enter a title"
-            }
-        });
+        rules: {
+            name: "required"
+        },
+        messages: {
+            name: "Please enter a title"
+        }, submitHandler: function(form)
+        {
+            $.post(site_url + '/transmission/add_transmission', $('#add_transmission_form').serialize(), function(msg)
+            {
+                if (msg == 1) {
+                    $("#add_project_msg").html('<div class="alert alert-success"><button class="close" data-dismiss="alert"></button>Success: The <a class="link" >project </a>has been added.</div>');
+                    add_project_form.reset();
+                    window.location = site_url + '/project/project_controller/manage_projects';
+                } else {
+                    $("#add_project_msg").html('<div class="alert alert-error"><button class="close" data-dismiss="alert"></button>Error: The <a class="link" href="#">project </a>has failed.</div>');
+                }
+            });
+
+
+        }
+    });
 });
 
