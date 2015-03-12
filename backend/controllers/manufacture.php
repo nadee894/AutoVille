282968aab319139ae22b42ba4b39e394dfa3be54
Ishani Pathinayake <ishanipathinayake@gmail.com>
2015-03-13 02:26:30 +0530

    Manufacture Add button

--- backend/controllers/manufacture.php
+++ backend/controllers/manufacture.php
@@ -19,11 +19,31 @@ class Manufacture extends CI_Controller {
     function manage_manufactures() {
 
         $manufacture_service = new Manufacture_service();
+        
         $data['heading'] = "Manage Manufactures";
         $data['results'] = $manufacture_service->get_all_manufactures();
 
         $parials = array('content' => 'manufacture/manage_manufacture_view');
         $this->template->load('template/main_template', $parials, $data);
+    }
+
+    /*
+     * adding a manufactoure
+     */
+
+    function add_manufacture() {
+        $manufacture_model = new Manufacture_model();
+        $manufacture_service = new Manufacture_service();
+
+        $manufacture_model->set_name($this->input->post('name', TRUE));
+        $manufacture_model->set_description($this->input->post('description', TRUE));
+        $manufacture_model->set_added_by(1);
+        $manufacture_model->set_added_date(date("Y-m-d H:i:s"));
+        $manufacture_model->set_updated_by(1);
+        $manufacture_model->set_is_published('1');
+        $manufacture_model->set_is_deleted('0');
+
+        echo $manufacture_service->add_new_manufacture($manufacture_model);
     }
 
 }
--- backend/models/manufacture/manufacture_service.php
+++ backend/models/manufacture/manufacture_service.php
@@ -6,12 +6,20 @@ class Manufacture_service extends CI_Model {
         parent::__construct();
         $this->load->model('manufacture/manufacture_model');
     }
-    
+
+    /*
+     * Service function to add a new manufacture
+     */
+
+    function add_new_manufacture($manufacture_model) {
+        return $this->db->insert('manufacture', $manufacture_model);
+    }
+
     public function get_all_manufactures() {
 
         $this->db->select('*');
         $this->db->from('manufacture');
-        $this->db->where('is_deleted','0');
+        $this->db->where('is_deleted', '0');
         $this->db->order_by("added_date", "desc");
         $query = $this->db->get();
         return $query->result();
