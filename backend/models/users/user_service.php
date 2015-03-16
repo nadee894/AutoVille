dc7a99c30decb0b9439f7767ceba51c631b7295c
nadeesha894 <liyanage.nadeesha123@gmail.com>
2015-03-16 16:15:38 +0530

    Modifications

---
+++ backend/models/users/user_service.php
@@ -0,0 +1,18 @@
+<?php
+
+class User_service extends CI_Model{
+    
+    function __construct() {
+        parent::__construct();
+        $this->load->model('users/user_model');
+    }
+    
+    
+    function get_admin_details
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
