b02600db24538062dd7ed5c790252280e179bd8e
randyhesh <heshani7.herath@gmail.com>
2015-03-17 22:56:25 +0530

    login

--- backend/controllers/login.php
+++ backend/controllers/login.php
@@ -1,4 +1,5 @@
 <?php
+
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
@@ -9,12 +10,13 @@ class Login extends CI_Controller {
 
         $this->load->model('users/user_model');
         $this->load->model('users/user_service');
-       
     }
 
-    function load_login(){
+    function load_login() {
         if ($this->session->userdata('USER_LOGGED_IN')) {
-            $this->template->load('template/main_template');
+           
+            redirect(site_url() . '/dashboard');
+            //$this->template->load('template/main_template');
         } else {
             $this->template->load('template/login');
         }
@@ -22,18 +24,15 @@ class Login extends CI_Controller {
 
     //Login details checking function 
     function authenticate_user() {
-        
-        $user_model   = new User_model();
-        $user_service = new User_service();
 
-        $username = $this->input->post('login_username', TRUE);
+        $user_model = new User_model();
+        $user_service = new User_service();        
 
-        $user_model->set_user_name($username);
+        $user_model->set_user_name($this->input->post('login_username', TRUE));
         $user_model->set_password(md5($this->input->post('login_password', TRUE)));
-        
-        $result_user=$user_service->authenticate_user_with_password($user_model);
 
-        
+        $result_user = $user_service->authenticate_user_with_password($user_model);
+
         if (count($result_user) == 0) {
             $logged_user_result = false;
         } else {
@@ -50,18 +49,17 @@ class Login extends CI_Controller {
             $this->session->set_userdata('USER_EMAIL', $result_user->email);
             $this->session->set_userdata('USER_PROFILE_PIC', $result_user->profile_pic);
             $this->session->set_userdata('USER_ONLINE', 'Y');
-
             $this->session->set_userdata('USER_LOGGED_IN', 'TRUE');
-             echo 1;
-        }else{
+            
+            echo 1;
+        } else {
             echo 0;
         }
-             
     }
 
     function logout() {
 
-        $user_model   = new User_model();
+        $user_model = new User_model();
         $user_service = new User_service();
 
         $user_model->set_is_online('0');
