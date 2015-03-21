cd49087e818e132f0f70ab0d288a5429b1fa41b2
gayathma <gayathma3@gmail.com>
2015-03-21 16:12:38 +0530

    style

--- backend/views/body_type/manage_body_type_view.php
+++ backend/views/body_type/manage_body_type_view.php
@@ -23,8 +23,7 @@
                             <tr>
                                 <th>#</th>
                                 <th>Name</th>
-<!--                                <th>Added By</th>
-                                <th>Added Date</th>-->
+                                <!--<th>Logo</th>-->
                                 <th>Active Status</th>
                                 <th>Actions</th>
                             </tr>
@@ -37,8 +36,8 @@
                                 <tr id="body_type_<?php echo $result->id; ?>">
                                     <td><?php echo ++$i; ?></td>
                                     <td><?php echo $result->name; ?></td>
-    <!--                                    <td><?php echo $result->added_by_user; ?></td>
-                                    <td><?php echo $result->added_date; ?></td>-->
+                                    <!--<td align="center"><img src="<?php echo base_url(); ?>uploads/body_type_logo/<?php echo $result->logo; ?>" width="60px" /></td>-->
+
                                     <td align="center">
                                         <?php if ($result->is_published) { ?>
                                             <a class="btn btn-success btn-xs" onclick="change_publish_status(<?php echo $result->id; ?>, 0, this)" title="click to deactivate body type"><i class="fa fa-check"></i></a>
@@ -73,11 +72,67 @@
             </div>
             <form id="add_body_type_form" name="add_body_type_form">
                 <div class="modal-body">
+<!--                    <script src="<?php echo base_url(); ?>backend_resources/file_upload_plugin/ajaxupload.3.5.js" type="text/javascript"></script>
+                    <script>
+                                        //upload body type logo
+
+                                        $(function () {
+                                            var btnUpload = $('#upload');
+                                            var status = $('#status');
+                                            new AjaxUpload(btnUpload, {
+                                                action: '<?php echo site_url(); ?>/body_type/upload_body_type_logo',
+                                                name: 'uploadfile',
+                                                onSubmit: function (file, ext) {
+                                                    if (!(ext && /^(jpg|png|jpeg|gif)$/.test(ext))) {
+                                                        // extension is not allowed 
+                                                        status.text('Only JPG, PNG or GIF files are allowed');
+                                                        return false;
+                                                    }
+                                                    //status.text('Uploading...Please wait');
+                                                    //                                            $("#files").html("<i id='animate-icon' class='fa fa-spinner fa fa-2x fa-spin'></i>");
+
+                                                },
+                                                onComplete: function (file, response) {
+                                                    //On completion clear the status
+                                                    //status.text('');
+                                                    $("#files").html("");
+                                                    $("#sta").html("");
+                                                    //Add uploaded file to list
+                                                    if (response != "error") {
+                                                        $('#files').html("");
+                                                        $('<div></div>').appendTo('#files').html('<img src="<?php echo base_url(); ?>uploads/body_type_logo/' + response + '"   width="100px" height="68px" /><br />');
+                                                        picFileName = response;
+                                                        document.getElementById('logo').value = response;
+                                                        //                    document.getElementById('cover_image').value = response;
+                                                    } else {
+                                                        $('<div></div>').appendTo('#files').text(file).addClass('error');
+                                                    }
+                                                }
+                                            });
+
+                                        });
+                    </script>-->
 
                     <div class="form-group">
-                        <label for="name">Enter Body Type</label>
+                        <label for="name">Enter Body Type
+                             <span class="mandatory">*</span>
+                        </label>
+                       
                         <input id="name" class="form-control" name="name" type="text" placeholder="Body Type">
                     </div>
+                    <div class="form-group">
+                        <div id="upload">
+<!--
+                            <label class="form-label">Upload Logo</label>
+                            <button type="button" class="btn btn-info" id="browse">Browse</button>
+                            <input type="text" id="logo" name="logo" style="visibility: hidden" value=""/>
+                        </div>
+                        <div id="sta"><span id="status" ></span></div>
+                    </div>
+                    <div class="form-group">
+                        <div id="files" class="project-logo">
+                        </div>
+                    </div>-->
                     <span id="rtn_msg"></span>
                 </div>
                 <div class="modal-footer">
@@ -126,7 +181,7 @@
 
 
                     } else {
-                      $('#rtn_msg').html('<div class="alert alert-block alert-danger fade in"><button class="close close-sm" type="button" data-dismiss="alert"><i class="fa fa-times"></i></button><strong>An error occured.</strong></div>');
+                        $('#rtn_msg').html('<div class="alert alert-block alert-danger fade in"><button class="close close-sm" type="button" data-dismiss="alert"><i class="fa fa-times"></i></button><strong>An error occured.</strong></div>');
                     }
                 });
 
