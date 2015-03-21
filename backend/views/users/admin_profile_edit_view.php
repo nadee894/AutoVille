56af6a1ef811a5be90caf81bd97b8cfbadb4ffc6
nadeesha894 <liyanage.nadeesha123@gmail.com>
2015-03-21 15:26:21 +0530

    User profile

--- backend/views/users/admin_profile_edit_view.php
+++ backend/views/users/admin_profile_edit_view.php
@@ -1,5 +1,5 @@
 <h1> Profile Info</h1>
-<form class="form-horizontal" role="form">
+<form id="user_details" class="form-horizontal" role="form">
     <div class="form-group">
         <label  class="col-lg-2 control-label">About Me</label>
         <div class="col-lg-10">
@@ -9,55 +9,55 @@
     <div class="form-group">
         <label  class="col-lg-2 control-label">Title</label>
         <div class="col-lg-6">
-            <input name="" type="text" class="form-control" id="title" placeholder=" ">
+            <input name="title" type="text" class="form-control" id="title" placeholder=" ">
         </div>
     </div>
     <div class="form-group">
         <label  class="col-lg-2 control-label">Name</label>
         <div class="col-lg-6">
-            <input name="" type="text" class="form-control" id="name" placeholder=" ">
+            <input name="name" type="text" class="form-control" id="name" placeholder=" ">
         </div>
     </div>
     <div class="form-group">
         <label  class="col-lg-2 control-label">User Name</label>
         <div class="col-lg-6">
-            <input name="" type="text" class="form-control" id="username" placeholder=" ">
+            <input name="user_name" type="text" class="form-control" id="username" placeholder=" ">
         </div>
     </div>
     <div class="form-group">
         <label  class="col-lg-2 control-label">User Type</label>
         <div class="col-lg-6">
-            <input name="" type="text" class="form-control" id="type" placeholder=" ">
+            <input name="user_type" type="text" class="form-control" id="type" placeholder=" ">
         </div>
     </div>
     <div class="form-group">
         <label  class="col-lg-2 control-label">E-mail</label>
-         <div class="col-lg-6">
-            <input name="" type="text" class="form-control" id="email" placeholder=" ">
+        <div class="col-lg-6">
+            <input name="email" type="text" class="form-control" id="email" placeholder=" ">
         </div>
     </div>
     <div class="form-group">
         <label  class="col-lg-2 control-label">Address</label>
         <div class="col-lg-6">
-            <input name="" type="text" class="form-control" id="address" placeholder=" ">
+            <input name="address" type="text" class="form-control" id="address" placeholder=" ">
         </div>
     </div>
     <div class="form-group">
         <label  class="col-lg-2 control-label">Contact No 01</label>
         <div class="col-lg-6">
-            <input name="" type="text" class="form-control" id="mobile1" placeholder=" ">
+            <input name="contact_no_1" type="text" class="form-control" id="mobile1" placeholder=" ">
         </div>
     </div>
     <div class="form-group">
         <label  class="col-lg-2 control-label">Contact No 02</label>
         <div class="col-lg-6">
-            <input name="" type="text" class="form-control" id="mobile2" placeholder=" ">
+            <input name="contact_no_2" type="text" class="form-control" id="mobile2" placeholder=" ">
         </div>
     </div>
 
     <div class="form-group">
         <div class="col-lg-offset-2 col-lg-10">
-            <button type="submit" class="btn btn-success">Save</button>
+            <button type="submit" onclick="update_user_profile()" class="btn btn-success">Save</button>
             <button type="button" class="btn btn-default">Cancel</button>
         </div>
     </div>
@@ -70,26 +70,26 @@
                 <div class="form-group">
                     <label  class="col-lg-2 control-label">Current Password</label>
                     <div class="col-lg-6">
-                        <input type="password" class="form-control" id="c-pwd" placeholder=" ">
+                        <input name="c_password" type="password" class="form-control" id="c-pwd" placeholder=" ">
                     </div>
                 </div>
                 <div class="form-group">
                     <label  class="col-lg-2 control-label">New Password</label>
                     <div class="col-lg-6">
-                        <input type="password" class="form-control" id="n-pwd" placeholder=" ">
+                        <input name="" type="password" class="form-control" id="n-pwd" placeholder=" ">
                     </div>
                 </div>
                 <div class="form-group">
                     <label  class="col-lg-2 control-label">Re-type New Password</label>
                     <div class="col-lg-6">
-                        <input type="password" class="form-control" id="rt-pwd" placeholder=" ">
+                        <input name="n_pasword" type="password" class="form-control" id="rt-pwd" placeholder=" ">
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label  class="col-lg-2 control-label">Change Avatar</label>
                     <div class="col-lg-6">
-                        <input type="file" class="file-pos" id="exampleInputFile">
+                        <input name="profile_pic" type="file" class="file-pos" id="exampleInputFile">
                     </div>
                 </div>
 
@@ -105,6 +105,40 @@
 </section>
 
 <script type="text/javascript">
-    
-    
+
+    function update_user_profile() {
+        $("user_details").validate({
+            rules: {
+                title: "required",
+                name: "required",
+                user_name: "required",
+                user_type: "required",
+                email: "required",
+                address: "required",
+                contact_no_1: "required",
+                contact_no_2: "required"
+            }, messages: {
+                title: "Please enter a title",
+                name: "Please enter a name",
+                user_name: "Please enter a user name",
+                user_type: "Please enter a user type",
+                email: "Please enter a email",
+                address: "Please enter a address",
+                contact_no_1: "Please enter a contact number",
+                contact_no_2: "Please enter a contact number"
+            }, submitHandler: function (form)
+            {
+                $.post(site_url + '/user/update_user', $('#user_details').serialize(), function (msg)
+                {
+                    if (msg == 1) {
+                        window.location = site_url + '/user/manage_admins';
+                    } else {
+
+                    }
+                });
+
+
+            }
+        })
+    }
 </script>
