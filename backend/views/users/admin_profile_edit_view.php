8483d3630613c684d665d1ed9fd2b353577b65a1
nadeesha894 <liyanage.nadeesha123@gmail.com>
2015-03-22 16:07:15 +0530

    User profile

--- backend/views/users/admin_profile_edit_view.php
+++ backend/views/users/admin_profile_edit_view.php
@@ -1,143 +1,184 @@
 <h1> Profile Info</h1>
-<form id="edit_user_form" class="form-horizontal" role="form" method="POST" name="edit_user_form">
-    <div class="form-group">
-        <label  class="col-lg-2 control-label">About Me</label>
-        <div class="col-lg-10">
-            <textarea name="" id="" class="form-control" cols="30" rows="10"></textarea>
+<div class="modal-body">
+    <form id="edit_user_form" class="form-horizontal" role="form" method="POST" name="edit_user_form">
+        <div class="form-group">
+            <label  class="col-lg-2 control-label">About Me</label>
+            <div class="col-lg-10">
+                <textarea name="" id="" class="form-control" cols="30" rows="10"></textarea>
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <label  class="col-lg-2 control-label">Title</label>
-        <div class="col-lg-6">
-            <input name="title" type="text" class="form-control" id="title" placeholder=" ">
+        <div class="form-group">
+            <label  class="col-lg-2 control-label">Title</label>
+            <div class="col-lg-6">
+                <input name="title" type="text" class="form-control" id="title" placeholder=" ">
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <label  class="col-lg-2 control-label">Name</label>
-        <div class="col-lg-6">
-            <input name="name" type="text" class="form-control" id="name" placeholder=" ">
+        <div class="form-group">
+            <label  class="col-lg-2 control-label">Name</label>
+            <div class="col-lg-6">
+                <input name="name" type="text" class="form-control" id="name" placeholder=" ">
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <label  class="col-lg-2 control-label">User Name</label>
-        <div class="col-lg-6">
-            <input name="user_name" type="text" class="form-control" id="username" placeholder=" ">
+        <div class="form-group">
+            <label  class="col-lg-2 control-label">User Name</label>
+            <div class="col-lg-6">
+                <input name="user_name" type="text" class="form-control" id="username" placeholder=" ">
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <label  class="col-lg-2 control-label">User Type</label>
-        <div class="col-lg-6">
-            <input name="user_type" type="text" class="form-control" id="type" placeholder=" ">
+        <div class="form-group">
+            <label  class="col-lg-2 control-label">User Type</label>
+            <div class="col-lg-6">
+                <input name="user_type" type="text" class="form-control" id="type" placeholder=" ">
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <label  class="col-lg-2 control-label">E-mail</label>
-        <div class="col-lg-6">
-            <input name="email" type="text" class="form-control" id="email" placeholder=" ">
+        <div class="form-group">
+            <label  class="col-lg-2 control-label">E-mail</label>
+            <div class="col-lg-6">
+                <input name="email" type="text" class="form-control" id="email" placeholder=" ">
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <label  class="col-lg-2 control-label">Address</label>
-        <div class="col-lg-6">
-            <input name="address" type="text" class="form-control" id="address" placeholder=" ">
+        <div class="form-group">
+            <label  class="col-lg-2 control-label">Address</label>
+            <div class="col-lg-6">
+                <input name="address" type="text" class="form-control" id="address" placeholder=" ">
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <label  class="col-lg-2 control-label">Contact No 01</label>
-        <div class="col-lg-6">
-            <input name="contact_no_1" type="text" class="form-control" id="mobile1" placeholder=" ">
+        <div class="form-group">
+            <label  class="col-lg-2 control-label">Contact No 01</label>
+            <div class="col-lg-6">
+                <input name="contact_no_1" type="text" class="form-control" id="mobile1" placeholder=" ">
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <label  class="col-lg-2 control-label">Contact No 02</label>
-        <div class="col-lg-6">
-            <input name="contact_no_2" type="text" class="form-control" id="mobile2" placeholder=" ">
+        <div class="form-group">
+            <label  class="col-lg-2 control-label">Contact No 02</label>
+            <div class="col-lg-6">
+                <input name="contact_no_2" type="text" class="form-control" id="mobile2" placeholder=" ">
+            </div>
         </div>
-    </div>
 
-    <div class="form-group">
-        <div class="col-lg-offset-2 col-lg-10">
-            <button type="submit"  class="btn btn-success">Save</button>
-            <button type="button" class="btn btn-default">Cancel</button>
+        <div class="form-group">
+            <label  class="col-lg-2 control-label">Current Password</label>
+            <div class="col-lg-6">
+                <input name="c_password" type="password" class="form-control" id="c-pwd" placeholder=" ">
+            </div>
         </div>
-    </div>
-</form>
-<section>
-    <div class="panel panel-primary">
-        <div class="panel-heading"> Sets New Password & Avatar</div>
-        <div class="panel-body">
-            <form class="form-horizontal" role="form">
-                <div class="form-group">
-                    <label  class="col-lg-2 control-label">Current Password</label>
-                    <div class="col-lg-6">
-                        <input name="c_password" type="password" class="form-control" id="c-pwd" placeholder=" ">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label  class="col-lg-2 control-label">New Password</label>
-                    <div class="col-lg-6">
-                        <input name="" type="password" class="form-control" id="n-pwd" placeholder=" ">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label  class="col-lg-2 control-label">Re-type New Password</label>
-                    <div class="col-lg-6">
-                        <input name="n_pasword" type="password" class="form-control" id="rt-pwd" placeholder=" ">
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label  class="col-lg-2 control-label">Change Avatar</label>
-                    <div class="col-lg-6">
-                        <input name="profile_pic" type="file" class="file-pos" id="exampleInputFile">
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="col-lg-offset-2 col-lg-10">
-                        <button type="submit" class="btn btn-info">Save</button>
-                        <button type="button" class="btn btn-default">Cancel</button>
-                    </div>
-                </div>
-            </form>
+        <div class="form-group">
+            <label  class="col-lg-2 control-label">New Password</label>
+            <div class="col-lg-6">
+                <input name="" type="password" class="form-control" id="n-pwd" placeholder=" ">
+            </div>
         </div>
-    </div>
-</section>
+        <div class="form-group">
+            <label  class="col-lg-2 control-label">Re-type New Password</label>
+            <div class="col-lg-6">
+                <input name="n_pasword" type="password" class="form-control" id="rt-pwd" placeholder=" ">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label  class="col-lg-2 control-label">Change Avatar</label>
+            <div class="col-lg-6">
+                <input name="profile_pic" type="file" class="file-pos" id="exampleInputFile">
+            </div>
+        </div>
+
+
+
+        <div class="form-group">
+            <div class="col-lg-offset-2 col-lg-10">
+                <button type="submit"  class="btn btn-success">Save</button>
+                <button type="button" class="btn btn-default">Cancel</button>
+            </div>
+        </div>
+    </form>
+</div>
+
 
 <script type="text/javascript">
 
     $('#user_menu').addClass('active open');
+
     $(document).ready(function () {
         $("#edit_user_form").validate({
             rules: {
-                title: "required",
-                name: "required",
+                title: {
+                    required: true
+                },
+                name: {
+                    required: true
+                },
                 user_name: "required",
-                user_type: "required",
-                email: "required",
+                user_type: {
+                    required: true,
+                    digits: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
                 address: "required",
-                contact_no_1: "required",
-                contact_no_2: "required",
+                contact_no_1: {
+                    required: true,
+                    digits: true,
+                    minlength: 10,
+                    maxlength: 10
+                },
+                contact_no_2: {
+                    required: true,
+                    digits: true,
+                    minlength: 10,
+                    maxlength: 10
+                },
                 password: "required",
-                re_pasword: "required"
+                re_pasword: {
+                    required: true,
+                    equalTo: '#password'
+                }
+
 
             },
             messages: {
-                title: "Please enter a title",
-                name: "Please enter a name",
+                title: {
+                    required: "Please enter a title"
+                },
+                name: {
+                    required: "Please enter a name"
+                },
                 user_name: "Please enter a user name",
-                user_type: "Please enter a user type",
-                email: "Please enter a email",
+                user_type: {
+                    required: "Please enter a user type",
+                    digits: "Invalid user type"
+                },
+                email: {
+                    required: "Please enter a email",
+                    email: "Invalid Email"
+                },
                 address: "Please enter a address",
-                contact_no_1: "Please enter a contact number",
-                contact_no_2: "Please enter a contact number",
+                contact_no_1: {
+                    required: "Please enter a email",
+                    digits: "Enter numbers only",
+                    maxlength: "Phone number is too long",
+                    minlength: "Phone number is too short"
+                },
+                contact_no_2: {
+                    required: "Please enter a email",
+                    digits: "Enter numbers only",
+                    maxlength: "Phone number is too long",
+                    minlength: "Phone number is too short"
+                },
                 password: "Please enter a password",
-                re_pasword: "Retype the Password"
+                re_pasword:
+                        {
+                            required: "Retype the Password",
+                            equalTo: "Passwords are not matching"
+                        }
             }, submitHandler: function (form)
             {
                 $.post(site_url + '/users/update_user', $('#edit_user_form').serialize(), function (msg)
                 {
+                    alert(msg)
                     if (msg == 1) {
+
                         toastr.success("Profile Successfully updated !!", "AutoVille");
                         $('#rtn_msg').html('<div class="alert alert-success fade in"><button class="close close-sm" type="button" data-dismiss="alert"><i class="fa fa-times"></i></button><strong>Successfully saved!!.</strong></div>');
                         edit_user_form.reset();
