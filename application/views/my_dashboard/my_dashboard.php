9bbc1713e7545c6390915dc3b1e9f9ad14be95b0
gayathma <gayathma3@gmail.com>
2015-05-10 22:42:27 +0530

    zcz

--- application/views/my_dashboard/my_dashboard.php
+++ application/views/my_dashboard/my_dashboard.php
@@ -4,29 +4,31 @@
         <div class="dashboard-wrapper">
             <br>
             <div class="row">
-                <div id="sticker" class="col-md-3 col-sm-4">
-                    <!--                    <div>
-                                            <img class="img-responsive img-circle" src="<?php echo base_url() . 'uploads/user_avatars/avatar.png'; ?>"/>
-                                            <div id="upload">
-                                                <button type="button" class="btn btn-primary btn-small" id="browse"><i class="fa fa-camera"></i></button>
-                    
-                                            </div>
-                                            <div id="sta"><span id="status" ></span></div>
-                                        </div>-->
+                <div id="sticker">
+                    <div  class="col-md-3 col-sm-4">
+                        <!--                    <div>
+                                                <img class="img-responsive img-circle" src="<?php echo base_url() . 'uploads/user_avatars/avatar.png'; ?>"/>
+                                                <div id="upload">
+                                                    <button type="button" class="btn btn-primary btn-small" id="browse"><i class="fa fa-camera"></i></button>
+                        
+                                                </div>
+                                                <div id="sta"><span id="status" ></span></div>
+                                            </div>-->
 
-                    <!-- SIDEBAR -->
-                    <div class="users-sidebar tbssticky">
-                        <ul class="list-group">
-                            <li class="list-group-item"> <span class="badge">5</span> <a class="dashboard_link" href="#"><i class="fa fa-home"></i> Dashboard</a></li>
-                            <li class="list-group-item"> <span class="badge">5</span> <a id="searched_view" href="#"><i class="fa fa-star-o"></i> Saved Searches</a></li>
-                            <li class="list-group-item"> <span class="badge"></span> <a id="compare_vehicle_view" href="#"><i class="fa fa-folder-o"></i> Compare Vehicles</a></li>
-                            <li class="list-group-item"> <a href="<?php echo site_url(); ?>/vehicle_advertisements/post_new_advertisement"><i class="fa fa-plus-square-o"></i> Create new Advertisement</a></li>
-                            <li class="list-group-item active"> <span class="badge">2</span> <a class="dashboard_link" href="#"><i class="fa fa-edit"></i> My Advertisements</a></li>
-                            <li class="list-group-item"> <a id="profile_link" href="#" ><i class="fa fa-user"></i> My Profile</a></li>
-                            <li class="list-group-item"> <a href="user-dashboard-settings.html"><i class="fa fa-cog"></i> Account Settings</a></li>
+                        <!-- SIDEBAR -->
+                        <div class="users-sidebar tbssticky">
+                            <ul class="list-group">
+                                <li class="list-group-item"> <span class="badge">5</span> <a class="dashboard_link" href="#"><i class="fa fa-home"></i> Dashboard</a></li>
+                                <li class="list-group-item"> <span class="badge">5</span> <a id="searched_view" href="#"><i class="fa fa-star-o"></i> Saved Searches</a></li>
+                                <li class="list-group-item"> <span class="badge"></span> <a id="compare_vehicle_view" href="#"><i class="fa fa-folder-o"></i> Compare Vehicles</a></li>
+                                <li class="list-group-item"> <a href="<?php echo site_url(); ?>/vehicle_advertisements/post_new_advertisement"><i class="fa fa-plus-square-o"></i> Create new Advertisement</a></li>
+                                <li class="list-group-item active"> <span class="badge">2</span> <a class="dashboard_link" href="#"><i class="fa fa-edit"></i> My Advertisements</a></li>
+                                <li class="list-group-item"> <a id="profile_link" href="#" ><i class="fa fa-user"></i> My Profile</a></li>
+                                <li class="list-group-item"> <a href="user-dashboard-settings.html"><i class="fa fa-cog"></i> Account Settings</a></li>
 
-                        </ul>
-                    </div>
+                            </ul>
+                        </div>
+                    </div
                 </div>
                 <div class="col-md-9 col-sm-8" id="dashboard_right_content">
                     <?php echo $this->load->view('my_dashboard/my_advertisements'); ?>
@@ -37,8 +39,8 @@
 </div>
 
 <script>
-    $(window).load(function(){
-//      $("#sticker").sticky({ topSpacing: 100 });
+    $(document).ready(function() {
+        $("#sticker").sticky({topSpacing: 100});
     });
 
     //load profile view
