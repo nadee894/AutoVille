8ce05c3ad552309bf7b878669ada041c24aa728e
nadeesha <nadeesha@nadeesha-X555LA>
2015-05-11 15:39:58 +0530

    Featured vehicles

--- backend/models/vehicle_advertisments/vehicle_advertisments_service.php
+++ backend/models/vehicle_advertisments/vehicle_advertisments_service.php
@@ -127,5 +127,31 @@ class Vehicle_advertisments_service extends CI_Model {
         return $query->row();
     }
     
+    
+     /*
+     * Get approved advertisments to featured advertisement 
+     * Author - Nadeesha
+     * 
+     */
+    
+    function get_approved_advertisements_for_featured($limit) {
+
+        $this->db->select('vehicle_advertisements.*,user.name as added_by_user,'
+                . 'manufacture.name as manufacture,model.name as model,'
+                . 'transmission.name as transmission,fuel_type.name as fuel_type,'
+                . 'body_type.name as body_type');
+        $this->db->from('vehicle_advertisements');
+        $this->db->join('manufacture', 'manufacture.id = vehicle_advertisements.manufacture_id');
+        $this->db->join('model', 'model.id = vehicle_advertisements.model_id');
+        $this->db->join('transmission', 'transmission.id = vehicle_advertisements.transmission_id');
+        $this->db->join('fuel_type', 'fuel_type.id = vehicle_advertisements.fuel_type_id');
+        $this->db->join('body_type', 'body_type.id = vehicle_advertisements.body_type_id');
+        $this->db->join('user', 'user.id = vehicle_advertisements.added_by');
+        $this->db->where('vehicle_advertisements.is_deleted', '0');
+        $this->db->where('vehicle_advertisements.is_published', '1');
+        $this->db->order_by("vehicle_advertisements.added_date", "desc");
+        $query = $this->db->get();
+        return $query->result();
+    }
 
 }
