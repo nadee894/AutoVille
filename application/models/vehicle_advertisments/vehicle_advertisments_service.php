e115b28f267f9c062fc98f6912dc74df5d523038
ashanidiaz <ashanidiaz@gmail.com>
2015-05-11 12:58:44 -0700

    gyg

--- application/models/vehicle_advertisments/vehicle_advertisments_service.php
+++ application/models/vehicle_advertisments/vehicle_advertisments_service.php
@@ -333,4 +333,28 @@ class Vehicle_advertisments_service extends CI_Model {
         $query = $this->db->get();
         return $query->result();
     }
+    
+    function get_price_drop_vehicles($limit){
+        
+        $this->db->select('vehicle_advertisements.*,vehicle_images.image_path,'
+                . 'manufacture.name as manufacture,model.name as model,'
+                . 'transmission.name as transmission,fuel_type.name as fuel_type,'
+                . 'body_type.name as body_type');
+        $this->db->from('vehicle_advertisements');
+        $this->db->join('manufacture', 'manufacture.id = vehicle_advertisements.manufacture_id');
+        $this->db->join('model', 'model.id = vehicle_advertisements.model_id');
+        $this->db->join('transmission', 'transmission.id = vehicle_advertisements.transmission_id');
+        $this->db->join('fuel_type', 'fuel_type.id = vehicle_advertisements.fuel_type_id');
+        $this->db->join('body_type', 'body_type.id = vehicle_advertisements.body_type_id');
+        $this->db->join('vehicle_images', 'vehicle_images.vehicle_id = vehicle_advertisements.id');
+        $this->db->where('vehicle_advertisements.is_deleted', '0');
+        $this->db->where('vehicle_advertisements.is_price_drop','1');
+        $this->db->group_by('vehicle_advertisements.id');
+        if ($limit != '') {
+            $this->db->limit($limit);
+        }
+
+        $query = $this->db->get();
+        return $query->result();
+    }
 }
