7f7305effee3eacc68c6b74aef3a0027e22d935b
randyhesh <heshani7.herath@gmail.com>
2015-05-11 12:58:13 +0530

    price drop

--- application/views/vehicle_adds/price_drop_vehicles.php
+++ application/views/vehicle_adds/price_drop_vehicles.php
@@ -1,16 +1,8 @@
-<!--Hero Image-->
-<section class="hero-image search-filter-bottom">
-    <?php echo $this->load->view('vehicle_adds/load_vehicle_sepecs_for_search'); ?>
-</section>
-<!--end Hero Image-->
-
-<!--Featured-->
-<!--                        <section id="featured" class="block background-color-grey-dark equal-height">
-    <div class="container">
-        <header><h2>Featured</h2></header>
+<div class="container">
+        <header><h2>Price Drop</h2></header>
         <div class="row">
             <div class="col-md-3 col-sm-3">
-                <div class="item featured">
+                <div class="item">
                     <div class="image">
                         <div class="quick-view"><i class="fa fa-eye"></i><span>Quick View</span></div>
                         <a href="car-item-detail.html">
@@ -28,7 +20,7 @@
                             <div class="icon">
                                 <i class="fa fa-thumbs-up"></i>
                             </div>
-                            <img src="<?php echo base_url(); ?>application_resources/assets/img/items/cars/1.jpg" alt="">
+                            <img src="<?php echo base_url(); ?>application_resources/assets/img/items/cars/4.jpg" alt="">
                         </a>
                     </div>
                     <div class="wrapper">
@@ -46,12 +38,11 @@
                             </dl>
                         </div>
                     </div>
-                </div>
-                 /.item
+                </div>                 
             </div>
-            /.col-sm-4
+            
             <div class="col-md-3 col-sm-3">
-                <div class="item featured">
+                <div class="item">
                     <div class="image">
                         <div class="quick-view"><i class="fa fa-eye"></i><span>Quick View</span></div>
                         <a href="car-item-detail.html">
@@ -63,12 +54,12 @@
                                     </div>
                                 </div>
                             </div>
-                            <img src="<?php echo base_url(); ?>application_resources/assets/img/items/cars/3.jpg" alt="">
+                            <img src="<?php echo base_url(); ?>application_resources/assets/img/items/cars/11.jpg" alt="">
                         </a>
                     </div>
                     <div class="wrapper">
-                        <a href="car-item-detail.html"><h3>Mercedes Benz Class A</h3></a>
-                        <figure>63 Birch SUV</figure>
+                        <a href="car-item-detail.html"><h3>Land Rover Defender 110</h3></a>
+                        <figure>Off Road</figure>
                         <div class="price">$69.000</div>
                         <div class="info">
                             <dl>
@@ -81,12 +72,11 @@
                             </dl>
                         </div>
                     </div>
-                </div>
-                 /.item
+                </div>                 
             </div>
-            /.col-sm-4
+            
             <div class="col-md-3 col-sm-3">
-                <div class="item featured">
+                <div class="item">
                     <div class="image">
                         <div class="quick-view"><i class="fa fa-eye"></i><span>Quick View</span></div>
                         <a href="car-item-detail.html">
@@ -98,7 +88,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <img src="<?php echo base_url(); ?>application_resources/assets/img/items/cars/8.jpg" alt="">
+                            <img src="<?php echo base_url(); ?>application_resources/assets/img/items/cars/3.jpg" alt="">
                         </a>
                     </div>
                     <div class="wrapper">
@@ -116,12 +106,11 @@
                             </dl>
                         </div>
                     </div>
-                </div>
-                 /.item
+                </div>                 
             </div>
-            /.col-sm-4
+            
             <div class="col-md-3 col-sm-3">
-                <div class="item featured">
+                <div class="item">
                     <div class="image">
                         <div class="quick-view"><i class="fa fa-eye"></i><span>Quick View</span></div>
                         <a href="car-item-detail.html">
@@ -136,11 +125,11 @@
                             <div class="item-specific">
                                 <span>Used Car</span>
                             </div>
-                            <img src="<?php echo base_url(); ?>application_resources/assets/img/items/cars/7.jpg" alt="">
+                            <img src="<?php echo base_url(); ?>application_resources/assets/img/items/cars/10.jpg" alt="">
                         </a>
                     </div>
                     <div class="wrapper">
-                        <a href="car-item-detail.html"><h3>Infinity Q50</h3></a>
+                        <a href="car-item-detail.html"><h3>Hyundai i30 1.8</h3></a>
                         <figure>Sedan</figure>
                         <div class="price">$35.500</div>
                         <div class="info">
@@ -154,60 +143,7 @@
                             </dl>
                         </div>
                     </div>
-                </div>
-                 /.item
-            </div>
-            /.col-sm-4
-        </div>
-    </div>-->
-</section>
-<!--end Featured-->
-
-
-<!--Featured Vehicles-->
-<!--Featured-->
-<section id="featured" class="block background-color-grey-dark equal-height">
-    <?php echo $this->load->view('vehicle_adds/featured_vehicles'); ?>    
-</section>
-<!--end Featured-->
-
-
-<!--Categories-->
-<section id="manufacturer_list" class="block background-color-white">
-    <?php echo $this->load->view('manufacturers/manufacture_list_view'); ?>
-</section>
-<!--end Categories-->
-
-
-
-<!--Price Drop-->
-<section id="price-drop" class="block equal-height">
-    <?php echo $this->load->view('vehicle_adds/price_drop_vehicles'); ?>
-</section>
-<!--end Price Drop-->
-
-<!--Recent-->
-<section id="recent" class="block">
-    <?php echo $this->load->view('vehicle_adds/recent_adds'); ?>
-</section>
-<!--end Recent-->
-
-<!--Why Us-->
-<section id="why-us" class="block background-color-grey-dark">
-    <?php echo $this->load->view('content_pages/why_us'); ?>
-</section>
-<!--end Why Us-->
-
-<!--Partners-->
-<section id="partners" class="block">
-    <div class="container">
-        <header><h2>Partners</h2></header>
-        <div class="logos">
-            <?php foreach ($logos as $logo) { ?>
-                <div class="logo"><a href=""><img src="<?php echo base_url() . 'uploads/manufacture_logo/' . $logo->logo; ?>" width="76" alt=""></a></div>                
-                    <?php } ?>
-        </div>
+                </div>                 
+            </div>            
+        </div>        
     </div>
-    <!--/.container-->
-</section>
-<!--end Partners-->
