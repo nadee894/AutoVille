e55086ddf7f9a169c6ba2b12ffac4c22f6fa261a
randyhesh <heshani7.herath@gmail.com>
2015-05-12 12:05:11 +0530

    h

--- application/models/vehicle_compare/vehicle_compare_service.php
+++ application/models/vehicle_compare/vehicle_compare_service.php
@@ -15,7 +15,7 @@ class Vehicle_compare_service extends CI_Model {
     function get_vehicles_to_compare($user_id) {
         $this->db->select('vehicle_advertisements.*,manufacture.name as manufacture,model.name as model,vehicle_images.image_path');
         $this->db->from('vehicle_advertisements');
-        $this->db->where('vehicle_compare.user_id',$user_id);
+        $this->db->where('vehicle_compare.user_id', $user_id);
         $this->db->join('vehicle_compare', 'vehicle_advertisements.id=vehicle_compare.vehicle_id');
         $this->db->join('manufacture', 'manufacture.id = vehicle_advertisements.manufacture_id');
         $this->db->join('model', 'model.id = vehicle_advertisements.model_id');
@@ -24,8 +24,8 @@ class Vehicle_compare_service extends CI_Model {
         $query = $this->db->get();
         return $query->result();
     }
-    
-    function get_vehicle_to_compare_for_user($user_id){
+
+    function get_vehicle_to_compare_for_user($user_id) {
         $this->db->select('vehicle_advertisements.*,vehicle_images.image_path,'
                 . 'manufacture.name as manufacture,model.name as model,'
                 . 'transmission.name as transmission,fuel_type.name as fuel_type,'
@@ -36,32 +36,55 @@ class Vehicle_compare_service extends CI_Model {
         $this->db->join('model', 'model.id = vehicle_advertisements.model_id');
         $this->db->join('transmission', 'transmission.id = vehicle_advertisements.transmission_id');
         $this->db->join('fuel_type', 'fuel_type.id = vehicle_advertisements.fuel_type_id');
-        $this->db->join('body_type', 'body_type.id = vehicle_advertisements.body_type_id');        
-        $this->db->join('vehicle_images', 'vehicle_images.vehicle_id = vehicle_advertisements.id');            
+        $this->db->join('body_type', 'body_type.id = vehicle_advertisements.body_type_id');
+        $this->db->join('vehicle_images', 'vehicle_images.vehicle_id = vehicle_advertisements.id');
         $this->db->where('vehicle_advertisements.is_deleted', '0');
         $this->db->where('vehicle_advertisements.is_published', '1');
-        $this->db->where('vehicle_compare.user_id',$user_id);
+        $this->db->where('vehicle_compare.user_id', $user_id);
         $this->db->group_by('vehicle_advertisements.id');
-        
+
         $query = $this->db->get();
         return $query->result();
     }
 
-    
-    function delete_compared_vehicle($user_id,$vehicle_id){
-        return $this->db->delete('vehicle_compare', array('user_id' => $user_id,'vehicle_id'=>$vehicle_id)); 
+    function delete_compared_vehicle($user_id, $vehicle_id) {
+        return $this->db->delete('vehicle_compare', array('user_id' => $user_id, 'vehicle_id' => $vehicle_id));
     }
-    
-    
+
     function get_vehicles_to_compare_for_unregistered_user($vehicle_id) {
         $this->db->select('vehicle_advertisements.*,manufacture.name as manufacture,model.name as model,vehicle_images.image_path');
         $this->db->from('vehicle_advertisements');
-        $this->db->where('vehicle_advertisements.id',$vehicle_id);        
+        $this->db->where('vehicle_advertisements.id', $vehicle_id);
         $this->db->join('manufacture', 'manufacture.id = vehicle_advertisements.manufacture_id');
         $this->db->join('model', 'model.id = vehicle_advertisements.model_id');
-        $this->db->join('vehicle_images', 'vehicle_images.vehicle_id = vehicle_advertisements.id');        
+        $this->db->join('vehicle_images', 'vehicle_images.vehicle_id = vehicle_advertisements.id');
         $query = $this->db->get();
         return $query->result();
     }
-    
+
+    function get_vehicle_to_compare_for_unregistered_user($id_arr=array()) {
+        $this->db->select('vehicle_advertisements.*,vehicle_images.image_path,'
+                . 'manufacture.name as manufacture,model.name as model,'
+                . 'transmission.name as transmission,fuel_type.name as fuel_type,'
+                . 'body_type.name as body_type');
+        $this->db->from('vehicle_advertisements');
+        $this->db->join('manufacture', 'manufacture.id = vehicle_advertisements.manufacture_id');
+        $this->db->join('model', 'model.id = vehicle_advertisements.model_id');
+        $this->db->join('transmission', 'transmission.id = vehicle_advertisements.transmission_id');
+        $this->db->join('fuel_type', 'fuel_type.id = vehicle_advertisements.fuel_type_id');
+        $this->db->join('body_type', 'body_type.id = vehicle_advertisements.body_type_id');
+        $this->db->join('vehicle_images', 'vehicle_images.vehicle_id = vehicle_advertisements.id');
+        $this->db->where('vehicle_advertisements.is_deleted', '0');
+        $this->db->where('vehicle_advertisements.is_published', '1');
+
+        foreach ($id_arr as $id) {
+            $this->db->or_where('vehicle_advertisements.id',$id);
+        }
+
+        $this->db->group_by('vehicle_advertisements.id');
+
+        $query = $this->db->get();
+        return $query->result();
+    }
+
 }
