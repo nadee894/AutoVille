ed0a0fa4a4717835d0019c48c43229c7f1cee944
randyhesh <heshani7.herath@gmail.com>
2015-08-24 12:19:19 +0530

    fp login

--- application/views/content_pages/login.php
+++ application/views/content_pages/login.php
@@ -73,8 +73,19 @@
                                                 url: site_url + '/login/reset_password',
                                                 data: $form.serialize(),
                                                 success: function(msg) {
-
-                                                    alert(msg);
+                                                    if (msg == '1') {
+                                                        $('#fade_valid_msg').html('<div class="alert alert-success"><i class="fa fa-check-circle fa-fw fa-lg"></i>Email Sent!!</div>');
+                                                        $('#fade_valid_msg').fadeIn();
+                                                        $('#fade_valid_msg').fadeOut(4000);
+                                                    } else if (msg == '2') {
+                                                        $('#fade_valid_msg').html('<div class="alert alert-success"><i class="fa fa-check-circle fa-fw fa-lg"></i>Email Not Sent!!</div>');
+                                                        $('#fade_valid_msg').fadeIn();
+                                                        $('#fade_valid_msg').fadeOut(4000);
+                                                    } else {
+                                                        $('#fade_valid_msg').html('<div class="alert alert-danger"><i class="fa fa-check-circle fa-fw fa-lg"></i>Invalid User or Email!!</div>');
+                                                        $('#fade_valid_msg').fadeIn();
+                                                        $('#fade_valid_msg').fadeOut(4000);
+                                                    }
                                                 }
                                             });
 
