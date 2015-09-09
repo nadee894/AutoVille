f6be75922a348b3e78fbba3e95da96b89baca261
gayathma <gayathma3@gmail.com>
2015-09-09 19:32:59 +0530

    main

--- application/views/template/main_template.php
+++ application/views/template/main_template.php
@@ -140,38 +140,7 @@
                                     <!--End Start New Arrivals-->
 
                                     <div class="col-md-4 col-sm-4">
-                                        <section>
-                                            <h2>Recent Reviews</h2>
-                                            <a href="car-item-detail.html#reviews" class="review small">
-                                                <h3>Max Five Lounge</h3>
-                                                <figure>4365 Bruce Street</figure>
-                                                <div class="info">
-                                                    <div class="rating" data-rating="4"></div>
-                                                    <div class="type">
-                                                        <i><img src="<?php echo base_url(); ?>application_resources/assets/icons/restaurants-bars/restaurants/restaurant.png" alt=""></i>
-                                                        <span>Restaurant</span>
-                                                    </div>
-                                                </div>
-                                                <p>
-                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non suscipit felis, sed sagittis tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras ac placerat mauris.
-                                                </p>
-                                            </a><!--review
-                                            <a href="car-item-detail.html#reviews" class="review small">
-                                                <h3>Saguaro Tavern</h3>
-                                                <figure>2476 Whispering Pines Circle</figure>
-                                                <div class="info">
-                                                    <div class="rating" data-rating="5"></div>
-                                                    <div class="type">
-                                                        <i><img src="<?php echo base_url(); ?>application_resources/assets/icons/restaurants-bars/restaurants/restaurant.png" alt=""></i>
-                                                        <span>Restaurant</span>
-                                                    </div>
-                                                </div>
-                                                <p>
-                                                    Pellentesque mauris. Proin sit amet scelerisque risus. Donec semper semper erat ut mollis curabitur
-                                                </p>
-                                            </a>
-                                            <!--review-->
-                                        </section>
+                                        
                                         <!--end Recent Reviews-->
                                     </div>
                                     <div class="col-md-4 col-sm-4">
