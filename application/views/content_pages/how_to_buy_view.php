3b6779fd4b8c62cf08b278d13a1e068caa70f498
Nadeesha Liyanage <IT13009700@my.sliit.lk>
2015-09-10 15:07:05 +0530

    How to buy

---
+++ application/views/content_pages/how_to_buy_view.php
@@ -0,0 +1,115 @@
+<section class="container">
+    <div class="row">
+        <!--Content-->
+        <div class="col-md-9">
+           <header>
+                <h1 class="page-title">How To Buy</h1>
+            </header>
+
+            <div id="main">
+
+                <!--step_illust-->
+                <div id="step_illust" class="clearfix">
+                    <ul class="clearfix">
+
+                        <!--step1-->
+                        <li>
+                            <dl>
+                                <dt><span>STEP1</span><br /><a href="#step1"><img src="img/step1.gif" width="113" height="108" alt="step1" /></a></dt>
+                                <dd><a href="#step1">Find Cars</a></dd>
+                            </dl>
+                        </li>
+                        <li class="arrow">
+                            <img src="img/arrow.gif" width="42" height="45" alt="arrow" />	</li>
+                        <!--//step1-->
+
+                        <!--step2-->
+                        <li>
+                            <dl>
+                                <dt><span>STEP2</span><br /><a href="#step2"><img src="img/step2.gif" width="113" height="108" alt="step1" /></a></dt>
+                                <dd><a href="#step2">Contact Us&nbsp;&amp;&nbsp;Order</a></dd>
+                            </dl>
+                        </li>
+                        <li class="arrow">
+                            <img src="img/arrow.gif" width="42" height="45" alt="arrow" />	</li>
+                        <!--//step2-->
+
+                        <!--step3-->
+                        <li>
+                            <dl>
+                                <dt><span>STEP3</span><br />
+                                <a href="#step3"><img src="img/step3.gif" width="113" height="108" alt="step1" /></a></dt>
+                                <dd><a href="#step3">Agreement,&nbsp;Payment,&nbsp;&amp;<br />Shipment</a></dd>
+                            </dl>
+                        </li>
+                        <li class="arrow">
+                            <img src="img/arrow.gif" width="42" height="45" alt="arrow" /></li>
+                        <!--//step3-->
+
+                        <!--step4-->
+                        <li>
+                            <dl>
+                                <dt><span>STEP4</span><br /><a href="#step4"><img src="img/step4.gif" width="113" height="108" alt="step1" /></a></dt>
+                                <dd><a href="#step4">Pickup&nbsp;&amp;&nbsp;DRIVE YOUR CAR</a></dd>
+                            </dl>
+                        </li>
+                        <!--//step4-->
+                    </ul>
+                </div>
+                <!--//step_illust-->
+
+
+                <!--step1~4-->
+
+                <!--step1-->
+                <a name="step1"></a>
+                <div class="step_box clearfix">
+                    <h2><span>Find Cars</span></h2>
+                    <div class="step_inner">
+                        <div class="txt">Goo-net Exchange is the best place to find the vehicle of your dreams - easily.
+                            Browsing is a great way to find a vehicle if you still aren’t sure what vehicle you want to buy. On Goo-net Exchange, you can browse by Make, Model, Body Type, Year, Price, etc.  From there you can narrow down your search to the exact car that you are looking for. If you know what you want, and want to find listings quickly, try searching. Searching will bring you straight to vehicle listings.</div>
+                        <div class="tumb"><img src="img/step1_thum.gif" width="265" height="220" alt="step1" /></div>
+                    </div>
+                </div>
+                <!--//step1-->
+
+                <!--step2-->
+                <a name="step2"></a>
+                <div class="step_box clearfix">
+                    <h2 class="step2"><span>Contact Us&nbsp;&amp;&nbsp;Order</span></h2>
+                    <div class="step_inner">
+                        <div class="txt">Once you have chosen a vehicle, you can use the "Contact Now" form at the right side of the vehicle listing page to contact us. You can also always use email, fax, Skype, and phone for inquiring and ordering. Email, fax, and Skype may be the easiest way to contact us, and you can save your time and money. Please make sure that your contact details are correct. Upon receiving your request, we will contact Goo dealers to make sure the vehicle in stock and hold the vehicle.</div>
+                        <div class="tumb"><img src="img/step2_thum.gif" width="264" height="314" alt="step2" /></div>
+                    </div>
+                </div>
+                <!--//step2-->
+
+
+                <!--step3-->
+                <a name="step3"></a>
+                <div class="step_box clearfix">
+                    <h2 class="step3"><span>Agreement,&nbsp;Payment,&nbsp;&amp;&nbsp;Shipment</span></h2>
+                    <div class="step_inner" style="font-size:115%; letter-spacing:0.5px;">
+                        Once you have decided to purchase the vehicle, contact us to arrange final payment, shipping, and pickup. It is important that you contact us before sending payment in order for us to secure the vehicle for you. Upon receiving your payment, we will complete all necessary paperwork for customs clearance and shipping arrangement, and then we will ship your vehicle. We want you to be confident when buying on Goo-net Exchange, so we strive to provide the best services and resources in place to help you make a safe and secure transaction.
+                    </div>
+                </div>
+                <!--//step3-->
+
+
+                <!--step4-->
+                <a name="step4"></a>
+                <div class="step_box clearfix">
+                    <h2 class="step4"><span>Pickup&nbsp;&amp;&nbsp;DRIVE YOUR CAR</span></h2>
+                    <div class="step_inner">
+                        <div class="img"><img src="img/step4_thum.gif" width="135" height="115" alt="step4" /></div>
+                        <div class="txt_right">Once you pick up the vehicle at the port, you have completed your purchase!! Buying a vehicle on Goo-net Exchange really is that easy. Just keep in mind, when you have questions, always contact us. We are here to ensure a safe purchase and your satisfaction.</div>
+                    </div>
+                </div>
+                <!--//step4-->
+
+                <!--//step1~4-->
+
+            </div><!-- //#main -->
+        </div>
+    </div>
+</section>
