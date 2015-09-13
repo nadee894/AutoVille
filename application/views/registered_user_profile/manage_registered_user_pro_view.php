b1eb007cdfe4796fa156e4f533fdea4c906e3c80
ashanidiaz <ashanidiaz@gmail.com>
2015-09-13 19:39:31 +0530

    reg user profile complete

--- application/views/registered_user_profile/manage_registered_user_pro_view.php
+++ application/views/registered_user_profile/manage_registered_user_pro_view.php
@@ -1,6 +1,6 @@
 <section>
     <div class="row">
-        <div class="col-md-9 col-sm-9">
+        <div class="col-md-9 col-sm-9" id="details_">
             <h3>
                 <i class="fa fa-user"></i>
                 Personal Info
@@ -14,31 +14,31 @@
                                 <div class="col-md-6 col-sm-6">
                                     <div class="form-group">
                                         <label for="title">Title <span style="color: red;">*</span></label>
-                                        <input id="title" class="form-control" type="text" value="<?php echo $user->title; ?>" name="title">
+                                        <input required id="title" class="form-control" type="text" value="<?php echo $user->title; ?>" name="title">
                                     </div>
                                 </div>
                                 <div class="col-md-6 col-sm-6">
                                     <div class="form-group">
                                         <label for="name">Name <span style="color: red;">*</span></label>
-                                        <input id="name" class="form-control" type="text" value="<?php echo $user->name; ?>" name="name">
+                                        <input required id="name" class="form-control" type="text" value="<?php echo $user->name; ?>" name="name">
                                     </div>
                                 </div>
                                 <div class="col-md-6 col-sm-6">
                                     <div class="form-group">
                                         <label for="contact_no_1">Mobile <span style="color: red;">*</span></label>
-                                        <input class="form-control" type="text" value="<?php echo $user->contact_no_1; ?>" id="contact_no_1" name="contact_no_1">
+                                        <input required class="form-control" type="phone" value="<?php echo $user->contact_no_1; ?>" id="contact_no_1" name="contact_no_1">
                                     </div>
                                 </div>
                                 <div class="col-md-6 col-sm-6">
                                     <div class="form-group">
                                         <label for="contact_no_2">Phone <span style="color: red;">*</span></label>
-                                        <input class="form-control" type="text" value="<?php echo $user->contact_no_2; ?>" id="contact_no_2" name="contact_no_2">
+                                        <input required class="form-control" type="phone" value="<?php echo $user->contact_no_2; ?>" id="contact_no_2" name="contact_no_2">
                                     </div>
                                 </div>
                                 <div class="col-md-6 col-sm-6">
                                     <div class="form-group">
                                         <label for="email">Email <span style="color: red;">*</span></label>
-                                        <input id="email" class="form-control" type="email" value="<?php echo $user->email; ?>" name="email">
+                                        <input required id="email" class="form-control" type="email" value="<?php echo $user->email; ?>" name="email">
                                     </div>
                                 </div>
 
@@ -54,7 +54,7 @@
                             </div>
                         </section>
                         <div class="form-group">
-                            <input validateItSubmitBtn="noSubmit" validateItCallBack="submitForm" type="button" class="btn btn-large btn-default" value="Save Changes" />
+                            <input type="submit" class="btn btn-large btn-default" value="Save Changes" />
                         </div>
                     </div>
                 </div>
@@ -62,43 +62,25 @@
         </div>
 
         <div class="col-md-3 col-sm-9">
-            <section>
-                <div class="col-md-12">
-                    <h3>
-                        <i class="fa fa-image"></i>
-                        Profile Picture
-                    </h3>
-                    <div id="profile-picture" class="profile-picture dropzone dz-clickable">
-                        <input type="file" name="file">
-                        <div class="dz-default dz-message">
-                            <span>Click or drop picture here</span>
-                        </div>
-                        <?php if ($user->profile_pic != '') { ?>
-                            <img alt="" src="<?php echo base_url(); ?>uploads/user_avatars/<?php echo $user->profile_pic; ?>">
-                        <?php } else { ?>
-                            <img alt="" src="<?php echo base_url(); ?>uploads/user_avatars/avatar.png">
-                        <?php } ?>
-                    </div>
-                </div>
-            </section>
+            
 
             <section>
                 <h3>
                     <i class="fa fa-asterisk"></i>
                     Password Change
                 </h3>
-                <form id="form-password" class="framed" action="?" method="post" role="form">
+                <form id="form-password" class="framed" method="post" role="form">
                     <div class="form-group">
                         <label for="current_pwd">Current Password</label>
-                        <input  class="form-control" type="password" id="current_pwd" name="current_pwd">
+                        <input required class="form-control" type="password" id="current_pwd" name="current_pwd">
                     </div>
                     <div class="form-group">
                         <label for="new_pwd">New Password</label>
-                        <input class="form-control" type="password" id="new_pwd" name="new_pwd">
+                        <input required class="form-control" type="password" id="new_pwd" name="new_pwd">
                     </div>
                     <div class="form-group">
                         <label for="confirm-new-password">Confirm New Password</label>
-                        <input  class="form-control" type="password" id="confirm_new_pwd" name="confirm_new_pwd">
+                        <input required class="form-control" type="password" id="confirm_new_pwd" name="confirm_new_pwd">
                     </div>
                     <div class="form-group">
                         <input class="btn btn-default" type="submit" value="Change Password" />
@@ -115,6 +97,20 @@
 <script type="text/javascript" src="<?php echo base_url(); ?>application_resources/assets/js/validateIt.js"></script>
 
 <script type="text/javascript">
+    
+    function showNotification(msg){
+        a=`<div class="notification_216" style="
+            position: fixed;
+            bottom: 10px;
+            left: 10px;
+            border: black solid 2px;
+            border-radius: 10px;
+            padding: 10px;
+            font-size: 22px;
+        ">`+msg+`</div>`;
+    $("#details_").prepend(a);
+    setTimeout(function(){ $(".notification_216").fadeOut(function(){ $(this).remove(); }); }, 3000);
+    }
     function submitForm() {
         var postData = $("#edit_reg_user_profile_form").serializeArray();
         var formURL = site_url + '/reg_user_profile/update_reg_user_profile';
@@ -129,24 +125,87 @@
                     success: function(data, textStatus, jqXHR)
                     {
                         //alert(data + ">>" + textStatus);
-                        if (data == "SUCCESS") {
+                        if (data == "SUCCESS" ) {
+                            
+                            showNotification("Successfully Updated")
                             toastr.success("Successfully Updated", "AutoVille");
                         }
-                        else
+                        else{
+                            showNotification("Error!")
                             toastr.error("Error!!", "AutoVille");
+                        }
 
                     },
                     error: function(jqXHR, textStatus, errorThrown) {
-
+                        showNotification("Error!")
                         toastr.error("Error!!", "AutoVille");
                     }
                 });
         //$("#edit_reg_user_profile_form").submit(); //Submit  the FORM
     }
 
+    function submitPasswordForm() {
+        var postData = $("#form-password").serializeArray();
+        var formURL = site_url + '/reg_user_profile/update_reg_user_profile_password';
+        //alert(formURL); 
+
+
+        $.ajax(
+                {
+                    url: formURL,
+                    type: "POST",
+                    data: postData,
+                    success: function(data, textStatus, jqXHR)
+                    {
+                        //alert(data + ">>" + textStatus);
+                        if (data == "SUCCESS" ) {
+                            
+                            showNotification("Successfully Updated")
+                            toastr.success("Successfully Updated", "AutoVille");
+                        }
+                        else{
+                            showNotification("Error!")
+                            toastr.error("Error!!", "AutoVille");
+                        }
+
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        showNotification("Error!")
+                        toastr.error("Error!!", "AutoVille");
+                    }
+                });
+        //$("#edit_reg_user_profile_form").submit(); //Submit  the FORM
+    }
+    
+        var password = document.getElementById("new_pwd")
+        , confirm_password = document.getElementById("confirm_new_pwd");
+
+      function validatePassword(){
+        if(password.value != confirm_password.value) {
+          confirm_password.setCustomValidity("Passwords Don't Match");
+        } else {
+          confirm_password.setCustomValidity('');
+        }
+      }
+
+      password.onchange = validatePassword;
+      confirm_password.onkeyup = validatePassword;
     $(document).ready(function() {
 
-
+        $('#form-password').submit(function(event){
+            // cancels the form submission
+            event.preventDefault();
+            submitPasswordForm();
+        // do whatever you want here
+        });
+        $('#edit_reg_user_profile_form').submit(function(event){
+            // cancels the form submission
+            event.preventDefault();
+            submitForm();
+        // do whatever you want here
+        });
+        
+        
         /*   
          $("#edit_reg_user_profile_form").submit(function(e)
          {
