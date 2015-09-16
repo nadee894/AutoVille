a328fb69037b841169241ffcf12fba774711bd89
Nadeesha Liyanage <IT13009700@my.sliit.lk>
2015-09-16 21:57:56 +0530

    Site map design

--- application/views/content_pages/site_map_view.php
+++ application/views/content_pages/site_map_view.php
@@ -1,101 +1,148 @@
+<div class="atcui-grid atcui-clearfix page-content-wrapper ">
 
-<section class="clearfix">
-        <h3 class="title">Top Cars</h3>
-        <div class="contents">
-            <ul>
-                <li class="icon-angle-right"><a href="/cars/toyota/">Toyota</a></li>
-                <li class="icon-angle-right"><a href="/cars/suzuki/">Suzuki</a></li>
-                <li class="icon-angle-right"><a href="/cars/honda/">Honda</a></li>
-                <li class="icon-angle-right"><a href="/cars/nissan/">Nissan</a></li>
-                <li class="icon-angle-right"><a href="/cars/mitsubishi/">Mitsubishi</a></li>
-                <li class="icon-angle-right"><a href="/cars/mercedes-benz/">Mercedes-Benz</a></li>
-                <li class="icon-angle-right"><a href="/cars/mazda/">Mazda</a></li>
-                <li class="icon-angle-right"><a href="/cars/daihatsu/">Daihatsu</a></li>
-                <li class="icon-angle-right"><a href="/cars/maruti-suzuki/">Maruti-Suzuki</a></li>
-                <li class="icon-angle-right"><a href="/cars/bmw/">BMW</a></li>
-                <li class="icon-angle-right"><a href="/cars/hyundai/">Hyundai</a></li>
-                <li class="icon-angle-right"><a href="/cars/land-rover/">Land Rover</a></li>
-            </ul>
-        </div>
-</section>
+    <div id="j_id_aw" class="atcui atcui-content  " data-qaid="">
+        <div id="j_id_au-j_id_aw-sitemap" class="parsys atcui-inline " data-qaid="aem-content-sitemap"><!-- BEGIN AEM INJECTION -->
 
-    
-    
-    
-    
-    
-    <div class="clearfix">
-        <h3 class="title small">Top Car Models</h3>
-        <div class="contents">
-            <ul>
-                <li class="icon-angle-right" title="Toyota Aqua"><a href="/cars/toyota/aqua/">Toyota Aqua</a></li>
-                <li class="icon-angle-right" title="Honda Fit"><a href="/cars/honda/fit/">Honda Fit</a></li>
-                <li class="icon-angle-right" title="Toyota HiAce"><a href="/cars/toyota/hiace/">Toyota HiAce</a></li>
-                <li class="icon-angle-right" title="Nissan Leaf"><a href="/cars/nissan/leaf/">Nissan Leaf</a></li>
-                <li class="icon-angle-right" title="Suzuki Stingray"><a href="/cars/suzuki/stingray/">Suzuki Stingray</a></li>
-                <li class="icon-angle-right" title="Suzuki Wagon R"><a href="/cars/suzuki/wagon-r/">Suzuki Wagon R</a></li>
-                <li class="icon-angle-right" title="Suzuki Every"><a href="/cars/suzuki/every/">Suzuki Every</a></li>
-                <li class="icon-angle-right" title="Toyota Prius"><a href="/cars/toyota/prius/">Toyota Prius</a></li>
-                <li class="icon-angle-right" title="Toyota Axio"><a href="/cars/toyota/axio/">Toyota Axio</a></li>
-                <li class="icon-angle-right" title="Honda Vezel"><a href="/cars/honda/vezel/">Honda Vezel</a></li>
-            </ul>
-        </div>
-      
-    </div>
-    <div class="columns large-3">
-        <h3 class="title">Top Locations</h3>
-        <div class="contents">
-            <ul>
-                <li class="icon-angle-right"><a href="/cars/colombo/colombo-1/">Colombo</a></li>
-                <li class="icon-angle-right"><a href="/cars/colombo/dehiwala-6/">Dehiwala</a></li>
-                <li class="icon-angle-right"><a href="/cars/gampaha/gampaha-7/">Gampaha</a></li>
-                <li class="icon-angle-right"><a href="/cars/kandy/kandy-7/">Kandy</a></li>
-                <li class="icon-angle-right"><a href="/cars/kurunegala/kurunegala-7/">Kurunegala</a></li>
-                <li class="icon-angle-right"><a href="/cars/colombo/kaduwela-6/">Kaduwela</a></li>
-                <li class="icon-angle-right"><a href="/cars/gampaha/kelaniya-6/">Kelaniya</a></li>
-                <li class="icon-angle-right"><a href="/cars/colombo/sri-jayawardanapura-kotte/">Sri Jayawardanapura Kotte</a></li>
-                <li class="icon-angle-right"><a href="/cars/gampaha/kadawatha-6/">Kadawatha</a></li>
-                <li class="icon-angle-right"><a href="/cars/colombo/kesbewa/">Kesbewa</a></li>
-                <li class="icon-angle-right"><a href="/cars/matara/matara-7/">Matara</a></li>
-                <li class="icon-angle-right"><a href="/cars/gampaha/negombo/">Negombo</a></li>
-                <li class="icon-angle-right"><a href="/cars/gampaha/wattala-6/">Wattala</a></li>
-                <li class="icon-angle-right"><a href="/cars/colombo/maharagama-6/">Maharagama</a></li>
-                <li class="icon-angle-right"><a href="/cars/galle/galle-7/">Galle</a></li>
-                <li class="icon-angle-right"><a href="/cars/colombo/moratuwa-6/">Moratuwa</a></li>
-                <li class="icon-angle-right"><a href="/cars/colombo/hanwella-6/">Hanwella</a></li>
-                <li class="icon-angle-right"><a href="/cars/gampaha/kandana-7/">Kandana</a></li>
-                <li class="icon-angle-right"><a href="/cars/gampaha/ja-ela-6/">Ja-Ela</a></li>
-                <li class="icon-angle-right"><a href="/cars/colombo/homagama-6/">Homagama</a></li>
-            </ul>
-        </div>
-    </div>
-    <div class="columns large-3">
-        <h3 class="title">Top Dealers</h3>
-        <div class="contents">
-            <ul>
-                <li class="icon-angle-right" title="CS Car Sales"><a href="/cs-car-sales/">CS Car Sales</a></li>
-                <li class="icon-angle-right" title="Dammika Motor Traders"><a href="/dammika-motor-traders/">Dammika Motor Traders</a></li>
-                <li class="icon-angle-right" title="Erosha Traders Pvt Ltd"><a href="/erosha-traders-pvt-ltd/">Erosha Traders Pvt Ltd</a></li>
-                <li class="icon-angle-right" title="Magni Holdings"><a href="/magni-holdings/">Magni Holdings</a></li>
-                <li class="icon-angle-right" title="Wasana Trading"><a href="/wasana-trading/">Wasana Trading</a></li>
-                <li class="icon-angle-right" title="US Car Sales"><a href="/us-car-sales/">US Car Sales</a></li>
-                <li class="icon-angle-right" title="Osaka Car Sales"><a href="/osaka-car-sales/">Osaka Car Sales</a></li>
-                <li class="icon-angle-right" title="Harin Trade Enterprises"><a href="/harin-trade-enterprises/">Harin Trade Enterprises</a></li>
-                <li class="icon-angle-right" title="Tharanga Enterprises"><a href="/tharanga-enterprises/">Tharanga Enterprises</a></li>
-                <li class="icon-angle-right" title="Kusum Car Sale"><a href="/kusum-car-sale/">Kusum Car Sale</a></li>
-                <li class="icon-angle-right" title="Sampatha Auto Mart"><a href="/sampatha-auto-mart/">Sampatha Auto Mart</a></li>
-                <li class="icon-angle-right" title="Niroshan Motor Traders"><a href="/niroshan-motor-traders/">Niroshan Motor Traders</a></li>
-                <li class="icon-angle-right" title="Pradeep Motors"><a href="/pradeep-motors/">Pradeep Motors</a></li>
-                <li class="icon-angle-right" title="Ruvisha Motor Traders"><a href="/ruvisha-motor-traders/">Ruvisha Motor Traders</a></li>
-                <li class="icon-angle-right" title="New Kaduwela Enterprises"><a href="/new-kaduwela-enterprises/">New Kaduwela Enterprises</a></li>
-                <li class="icon-angle-right" title="Gayuki Holdings"><a href="/gayuki-holdings/">Gayuki Holdings</a></li>
-                <li class="icon-angle-right" title="A A Auto Mart"><a href="/a-a-auto-mart/">A A Auto Mart</a></li>
-                <li class="icon-angle-right" title="Roomy Traders"><a href="/roomy-traders/">Roomy Traders</a></li>
-                <li class="icon-angle-right" title="ABL Motor"><a href="/abl-motor/">ABL Motor</a></li>
-                <li class="icon-angle-right" title="Lanka Auto Mart"><a href="/lanka-auto-mart/">Lanka Auto Mart</a></li>
-            </ul>
+            <div class="par parsys"><div class="title parbase section"><h1>Site Map</h1>
+                </div>
+                <div class="rawHtmlContent section"><p>Find information on used, new and certified cars on Autotrader. Read car reviews and articles, compare cars and find a dealer in your area. Begin your car search at the ultimate automotive marketplace.</p>
+
+                    <!-- column #1 -->
+                    <div class="atcui-column atcui-span-6">
+                        <h3><a href="/research/index.jsp">Research &amp; Compare</a></h3>
+
+                        <ul class="atcui-list atcui-bare">
+                            <li class="atcui-list-item"><a href="/research/new-cars/index.jsp">Research New Cars</a></li>
+                            <li class="atcui-list-item"><a href="/research/certified-cars/index.jsp">Research Certified Cars</a></li>
+                            <li class="atcui-list-item"><a href="/research/used-cars/index.jsp">Research Used Cars</a></li>
+                            <li class="atcui-list-item"><a href="/research/model_info/index.jsp">Model Information</a></li>
+                            <li class="atcui-list-item"><a href="/research/compare/index.jsp">Compare Models</a></li>
+                            <li class="atcui-list-item"><a href="/research/pricing/trade.jsp">Car Values</a></li>
+                            <li class="atcui-list-item"><a href="/research/configure/index.jsp">Build &amp; Price a New Car</a></li>
+                        </ul>
+
+                        <ul class="atcui-list atcui-bare">
+                            <li class="atcui-list-item"><a href="/car-news/car-types.xhtml">Research cars by Style</a>
+                                <ul class="atcui-list atcui-bare">
+                                    <li class="atcui-list-item"><a href="/car-news/family-cars.xhtml">Family Cars</a></li>
+                                    <li class="atcui-list-item"><a href="/car-news/green-cars.xhtml">Green Cars</a></li>
+                                    <li class="atcui-list-item"><a href="/car-news/car-tech.xhtml">Car Tech</a></li>
+                                    <li class="atcui-list-item"><a href="/car-news/car-deals.xhtml">Car Deals</a></li>
+                                    <li class="atcui-list-item"><a href="/car-news/trucks.xhtml">Trucks</a></li>
+                                </ul>
+                            </li>        
+                        </ul>
+
+                        <ul class="atcui-list atcui-bare">
+                            <li class="atcui-list-item"><a href="/research/research/index.jsp">Articles, Videos &amp; Reviews</a></li>
+                            <li class="atcui-list-item"><a href="/research/research/top-picks-results.jsp">Top Picks</a></li>
+                            <li class="atcui-list-item"><a href="/research/research/auto-shows-main.jsp">Auto Show Headquarters</a></li>
+                            <li class="atcui-list-item"><a href="/research/learn-about-cars/car-safety.jsp?rdpage=RSLT">Safety Information</a></li>
+                            <li class="atcui-list-item"><a href="/fraud/index.xhtml">Fraud Awareness</a></li>
+                        </ul>
+
+                        <h3><a href="https://www.roadloans.com/autotrader/" target="_blank">Car Loans</a></h3>
+
+                        <ul class="atcui-list atcui-bare">
+                            <li class="atcui-list-item"><a href="/calculators/?rdpage=SUBNAV">Auto Loan Calculator and Rates</a></li>
+                        </ul>
+
+                        <h3><a href="/insurance/index.xhtml">Car Insurance</a></h3>
+                    </div>
+
+                    <!-- column #2 -->
+                    <div class="atcui-column atcui-span-6">
+                        <h3><a href="/cars-for-sale/">Find Your Car</a></h3>
+
+                        <ul class="atcui-list atcui-bare">
+                            <li class="atcui-list-item"><a href="/cars-for-sale/index.xhtml?listingType=new&searchRadius=0">Find a New Car for Sale</a></li>
+                            <li class="atcui-list-item"><a href="/cars-for-sale/index.xhtml?listingType=certified&searchRadius=0">Find a Certified Car for Sale</a></li>
+                            <li class="atcui-list-item"><a href="/cars-for-sale/index.xhtml?listingType=used&searchRadius=0">Find a Used Car for Sale</a></li>
+                            <li class="atcui-list-item"><a href="http://www.autotraderlatino.com/">Encuentra un auto usado en espa&ntilde;ol</a></li>
+                        </ul>
+
+                        <ul class="atcui-list atcui-bare">
+                            <li class="atcui-list-item"><a href="/find/cars-for-sale.jsp">Browse Cars for Sale</a></li>
+                        </ul>
+
+                        <h3><a href="/car-dealers">Find Your Dealer</a></h3>
+
+                        <ul class="atcui-list atcui-bare">
+                            <li class="atcui-list-item"><a href="/find/car-dealers/">Browse Car Dealers</a></li>
+                        </ul>
+
+                        <h3><a href="/calculators/?rdpage=SUBNAV">Calculators</a></h3>
+
+                        <ul class="atcui-list atcui-bare">
+                            <li class="atcui-list-item"><a href="/calculators/?rdpage=SUBNAV">Auto Loan Calculator</a></li>
+                        </ul>
+                    </div>
+
+                    <!-- column #3 -->
+                    <div class="atcui-column atcui-span-6">
+                        <h3><a href="/sell-car/">Sell Your Car</a></h3>
+
+                        <ul class="atcui-list atcui-bare">
+                            <li class="atcui-list-item"><a href="https://sell.autotrader.com/syc/login.jsf">Edit Your Autotrader Private Seller Ad</a></li>
+                            <li class="atcui-list-item"><a href="/car-values/?restype=used">What's My Car Worth?</a></li>
+                            <li class="atcui-list-item"><a href="http://carsellingadviser.autotrader.com/rc/home.jsf">Learn to Sell a Car Yourself</a></li>
+                        </ul>
+
+                        <h3><a href="/myatc/">My Autotrader</a></h3>
+
+                        <ul class="atcui-list atcui-bare">
+                            <li class="atcui-list-item"><a href="/myatc/my_summary.xhtml">My Summary</a></li>
+                            <li class="atcui-list-item"><a href="/myatc/my_cars.xhtml">My Cars</a></li>
+                            <li class="atcui-list-item"><a href="/myatc/my_searches.xhtml">My Searches</a></li>
+                            <li class="atcui-list-item"><a href="/myatc/my_profile.xhtml">My Profile</a></li>
+                        </ul>
+
+                        <ul class="atcui-list atcui-bare">
+                            <li class="atcui-list-item"><a href="/myatc/register.xhtml">Join My Autotrader</a></li>
+                            <li class="atcui-list-item"><a href="/help/my-autotrader.xhtml">My Autotrader Help</a></li>
+                        </ul>
+
+                        <h3><a href="/help/index.xhtml">Help</a></h3>
+
+                        <ul class="atcui-list atcui-bare">
+                            <li class="atcui-list-item"><a href="/help/about-our-site.xhtml">Help with Our Site</a></li>
+                            <li class="atcui-list-item"><a href="/help/my-autotrader.xhtml">My Autotrader</a></li>
+                            <li class="atcui-list-item"><a href="/help/shopping-for-cars.xhtml">Shopping for Cars</a></li>
+                            <li class="atcui-list-item"><a href="/help/selling-your-car.xhtml">Selling Your Car</a></li>
+                            <li class="atcui-list-item"><a href="/help/car-loans-and-insurance.xhtml">Car Loans and Insurance</a></li>
+                            <li class="atcui-list-item"><a href="/help/technical-questions.xhtml">Technical Questions</a></li>
+                        </ul>
+
+                        <ul class="atcui-list atcui-bare">
+                            <li class="atcui-list-item"><a href="/help/contact.xhtml">Contact Us for Help</a></li>
+                            <li class="atcui-list-item"><a href="/help/glossary.jsp?letter=A">Glossary of Automotive Terms</a></li>
+                        </ul>
+                    </div>
+
+                    <!-- column #4 -->
+                    <div class="atcui-column atcui-span-6 atcui-last">
+                        <h3><a href="/about/index.xhtml">About Autotrader</a></h3>
+
+                        <ul class="atcui-list atcui-bare">
+                            <li class="atcui-list-item"><a href="/about/community-relations/index.xhtml">Community Relations</a></li>
+                            <li class="atcui-list-item"><a href="http://autotrader.mediaroom.com/">Press Room</a></li>
+                            <li class="atcui-list-item"><a href="/careers/index.jsp">Autotrader Jobs</a></li>
+                            <li class="atcui-list-item"><a href="https://dealers.autotrader.com/dc/portal/index.jsf">Become an Autotrader Dealer</a></li>
+                            <li class="atcui-list-item"><a href="http://digitalsolutions.autotrader.com/advertising-solutions/">Autotrader Ad Specs</a></li>
+                            <li class="atcui-list-item"><a href="/legal/visitor-agreement.xhtml" onclick="return false"><span id="visitorLink" class="atcui-trigger atcui-icon-link atcui-icon-span" data-atcui-trigger-target="popupVisitor" data-atcui-trigger-event="click" data-atcui-trigger-method="open">Visitor Agreement</span></a></li>
+                            <li class="atcui-list-item"><a href="/privacy.jsp">Privacy Statement</a></li>
+                            <li class="atcui-list-item"><a href="/new_features.jsp">New Features on ATC</a></li>
+                            <li class="atcui-list-item"><a href="http://digitalsolutionsautotrader.com/autotrader-offsite/">Autotrader Access</a></li>
+                        </ul>
+                    </div>
+
+                    <span data-atcui-role="popup" data-atcui-config="{&quot;id&quot;: &quot;popupVisitor&quot;, &quot;url&quot;: &quot;/legal/visitor-agreement.xhtml&quot;, &quot;resizable&quot;: true, &quot;width&quot;: 650, &quot;gridWidth&quot;: 0, &quot;height&quot;: 590, &quot;name&quot;: &quot;atcuiPopup&quot;, &quot;printable&quot;: false, &quot;exclusive&quot;: false}"></span></div>
+
+            </div>
+
+
+
+            <!-- END AEM INJECTION -->
         </div>
     </div>
 </div>
-
-
