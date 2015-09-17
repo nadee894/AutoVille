e5b1729014f286a9899b58f2c4c160da9a1d116d
Nadeesha Liyanage <IT13009700@my.sliit.lk>
2015-09-18 00:54:13 +0530

    bbb

--- application/views/content_pages/site_map_view.php
+++ application/views/content_pages/site_map_view.php
@@ -6,52 +6,47 @@
 
             <div class="par parsys"><div class="title parbase section"><h1>Site Map</h1>
                 </div>
-                <div class="rawHtmlContent section"><p>Find information on used, new and certified cars on Autotrader. Read car reviews and articles, compare cars and find a dealer in your area. Begin your car search at the ultimate automotive marketplace.</p>
+                <div class="rawHtmlContent section"><p>Find information on cars on Autoville. Read car reviews and articles, compare cars and find a car in your area. Begin your car search at the ultimate automotive marketplace.</p>
 
                     <div class="col-md-12 item">
 
                         <div class="col-md-4">
                             <!-- column #1 -->
                             <div class="atcui-column atcui-span-6">
-                                <h3><a href="/research/index.jsp">Research &amp; Compare</a></h3>
+                                <h3><a>Research &amp; Compare</a></h3>
 
                                 <ul class="atcui-list atcui-bare">
-                                    <li class="atcui-list-item"><a href="/research/new-cars/index.jsp">Research New Cars</a></li>
-                                    <li class="atcui-list-item"><a href="/research/certified-cars/index.jsp">Research Certified Cars</a></li>
-                                    <li class="atcui-list-item"><a href="/research/used-cars/index.jsp">Research Used Cars</a></li>
-                                    <li class="atcui-list-item"><a href="/research/model_info/index.jsp">Model Information</a></li>
-                                    <li class="atcui-list-item"><a href="/research/compare/index.jsp">Compare Models</a></li>
-                                    <li class="atcui-list-item"><a href="/research/pricing/trade.jsp">Car Values</a></li>
-                                    <li class="atcui-list-item"><a href="/research/configure/index.jsp">Build &amp; Price a New Car</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/vehicle_news/list_vehicle_news">Research New Cars</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/vehicle_search/search_advertisements">Research Used Cars</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/vehicle_search/search_advertisements">Model Information</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/vehicle_compare/load_vehicle_popup">Compare Vehicles</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/vehicle_search/search_advertisements">Car Values</a></li>
+
                                 </ul>
+
+                                <!--                                <ul class="atcui-list atcui-bare">
+                                                                    <li class="atcui-list-item"><a href="/car-news/car-types.xhtml">Research cars by Style</a>
+                                                                        <ul class="atcui-list atcui-bare">
+                                                                            <li class="atcui-list-item"><a href="/car-news/family-cars.xhtml">Family Cars</a></li>
+                                                                            <li class="atcui-list-item"><a href="/car-news/green-cars.xhtml">Green Cars</a></li>
+                                                                            <li class="atcui-list-item"><a href="/car-news/car-tech.xhtml">Car Tech</a></li>
+                                                                            <li class="atcui-list-item"><a href="/car-news/car-deals.xhtml">Car Deals</a></li>
+                                                                            <li class="atcui-list-item"><a href="/car-news/trucks.xhtml">Trucks</a></li>
+                                                                        </ul>
+                                                                    </li>        
+                                                                </ul>-->
 
                                 <ul class="atcui-list atcui-bare">
-                                    <li class="atcui-list-item"><a href="/car-news/car-types.xhtml">Research cars by Style</a>
-                                        <ul class="atcui-list atcui-bare">
-                                            <li class="atcui-list-item"><a href="/car-news/family-cars.xhtml">Family Cars</a></li>
-                                            <li class="atcui-list-item"><a href="/car-news/green-cars.xhtml">Green Cars</a></li>
-                                            <li class="atcui-list-item"><a href="/car-news/car-tech.xhtml">Car Tech</a></li>
-                                            <li class="atcui-list-item"><a href="/car-news/car-deals.xhtml">Car Deals</a></li>
-                                            <li class="atcui-list-item"><a href="/car-news/trucks.xhtml">Trucks</a></li>
-                                        </ul>
-                                    </li>        
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/website_comments/list_website_comments">Reviews</a></li>
                                 </ul>
+
+                                <h3><a target="_blank">Car Loans</a></h3>
 
                                 <ul class="atcui-list atcui-bare">
-                                    <li class="atcui-list-item"><a href="/research/research/index.jsp">Articles, Videos &amp; Reviews</a></li>
-                                    <li class="atcui-list-item"><a href="/research/research/top-picks-results.jsp">Top Picks</a></li>
-                                    <li class="atcui-list-item"><a href="/research/research/auto-shows-main.jsp">Auto Show Headquarters</a></li>
-                                    <li class="atcui-list-item"><a href="/research/learn-about-cars/car-safety.jsp?rdpage=RSLT">Safety Information</a></li>
-                                    <li class="atcui-list-item"><a href="/fraud/index.xhtml">Fraud Awareness</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/vehicle_search/search_advertisements">Auto Loan Calculator and Rates</a></li>
                                 </ul>
 
-                                <h3><a href="https://www.roadloans.com/autotrader/" target="_blank">Car Loans</a></h3>
-
-                                <ul class="atcui-list atcui-bare">
-                                    <li class="atcui-list-item"><a href="/calculators/?rdpage=SUBNAV">Auto Loan Calculator and Rates</a></li>
-                                </ul>
-
-                                <h3><a href="/insurance/index.xhtml">Car Insurance</a></h3>
+                                <!--<h3><a href="/insurance/index.xhtml">Car Insurance</a></h3>-->
                             </div>
 
                         </div>
@@ -62,26 +57,25 @@
                                 <h3><a href="/cars-for-sale/">Find Your Car</a></h3>
 
                                 <ul class="atcui-list atcui-bare">
-                                    <li class="atcui-list-item"><a href="/cars-for-sale/index.xhtml?listingType=new&searchRadius=0">Find a New Car for Sale</a></li>
-                                    <li class="atcui-list-item"><a href="/cars-for-sale/index.xhtml?listingType=certified&searchRadius=0">Find a Certified Car for Sale</a></li>
-                                    <li class="atcui-list-item"><a href="/cars-for-sale/index.xhtml?listingType=used&searchRadius=0">Find a Used Car for Sale</a></li>
-                                    <li class="atcui-list-item"><a href="http://www.autotraderlatino.com/">Encuentra un auto usado en espa&ntilde;ol</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/vehicle_search/search_advertisements">Find a New Car for Sale</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/vehicle_search/search_advertisements">Find a Used Car for Sale</a></li>
+
                                 </ul>
 
                                 <ul class="atcui-list atcui-bare">
-                                    <li class="atcui-list-item"><a href="/find/cars-for-sale.jsp">Browse Cars for Sale</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/vehicle_search/search_advertisements">Browse Cars for Sale</a></li>
                                 </ul>
 
-                                <h3><a href="/car-dealers">Find Your Dealer</a></h3>
+                                <!--                                <h3><a>Find Your Dealer</a></h3>
+                                
+                                                                <ul class="atcui-list atcui-bare">
+                                                                    <li class="atcui-list-item"><a href="/find/car-dealers/">Browse Car Dealers</a></li>
+                                                                </ul>-->
+
+                                <h3><a>Calculators</a></h3>
 
                                 <ul class="atcui-list atcui-bare">
-                                    <li class="atcui-list-item"><a href="/find/car-dealers/">Browse Car Dealers</a></li>
-                                </ul>
-
-                                <h3><a href="/calculators/?rdpage=SUBNAV">Calculators</a></h3>
-
-                                <ul class="atcui-list atcui-bare">
-                                    <li class="atcui-list-item"><a href="/calculators/?rdpage=SUBNAV">Auto Loan Calculator</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/vehicle_search/search_advertisements">Auto Loan Calculator</a></li>
                                 </ul>
                             </div>                        
 
@@ -90,23 +84,22 @@
                                 <h3><a href="/sell-car/">Sell Your Car</a></h3>
 
                                 <ul class="atcui-list atcui-bare">
-                                    <li class="atcui-list-item"><a href="https://sell.autotrader.com/syc/login.jsf">Edit Your Autotrader Private Seller Ad</a></li>
-                                    <li class="atcui-list-item"><a href="/car-values/?restype=used">What's My Car Worth?</a></li>
-                                    <li class="atcui-list-item"><a href="http://carsellingadviser.autotrader.com/rc/home.jsf">Learn to Sell a Car Yourself</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/vehicle_advertisements/post_new_advertisement">Post your ad</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/pages/how_to_buy">Learn to Sell a Car Yourself</a></li>
                                 </ul>
 
-                                <h3><a href="/myatc/">My Autotrader</a></h3>
+                                <h3><a href="/myatc/">My Autoville</a></h3>
 
                                 <ul class="atcui-list atcui-bare">
-                                    <li class="atcui-list-item"><a href="/myatc/my_summary.xhtml">My Summary</a></li>
-                                    <li class="atcui-list-item"><a href="/myatc/my_cars.xhtml">My Cars</a></li>
-                                    <li class="atcui-list-item"><a href="/myatc/my_searches.xhtml">My Searches</a></li>
-                                    <li class="atcui-list-item"><a href="/myatc/my_profile.xhtml">My Profile</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/dashboard">My Summary</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/dashboard">My Cars</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/dashboard">My Searches</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/dashboard">My Profile</a></li>
                                 </ul>
 
                                 <ul class="atcui-list atcui-bare">
-                                    <li class="atcui-list-item"><a href="/myatc/register.xhtml">Join My Autotrader</a></li>
-                                    <li class="atcui-list-item"><a href="/help/my-autotrader.xhtml">My Autotrader Help</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/register_users/load_registration">Join My Autoville</a></li>
+
                                 </ul>
 
                             </div>
@@ -116,7 +109,7 @@
                             <!-- column #4 -->
                             <div class="atcui-column atcui-span-6 atcui-last">
 
-                                <h3><a href="/help/index.xhtml">Help</a></h3>
+<!--                                <h3><a href="/help/index.xhtml">Help</a></h3>
 
                                 <ul class="atcui-list atcui-bare">
                                     <li class="atcui-list-item"><a href="/help/about-our-site.xhtml">Help with Our Site</a></li>
@@ -130,20 +123,20 @@
                                 <ul class="atcui-list atcui-bare">
                                     <li class="atcui-list-item"><a href="/help/contact.xhtml">Contact Us for Help</a></li>
                                     <li class="atcui-list-item"><a href="/help/glossary.jsp?letter=A">Glossary of Automotive Terms</a></li>
-                                </ul>
+                                </ul>-->
 
-                                <h3><a href="/about/index.xhtml">About Autotrader</a></h3>
+                                <h3><a>About Autotrader</a></h3>
 
                                 <ul class="atcui-list atcui-bare">
-                                    <li class="atcui-list-item"><a href="/about/community-relations/index.xhtml">Community Relations</a></li>
-                                    <li class="atcui-list-item"><a href="http://autotrader.mediaroom.com/">Press Room</a></li>
-                                    <li class="atcui-list-item"><a href="/careers/index.jsp">Autotrader Jobs</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/home/about_us">About Us</a></li>
+                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/pages/contact_us">Contact Us</a></li>
+<!--                                    <li class="atcui-list-item"><a href="<?php echo site_url(); ?>/register_users/load_registration">Autotrader Jobs</a></li>
                                     <li class="atcui-list-item"><a href="https://dealers.autotrader.com/dc/portal/index.jsf">Become an Autotrader Dealer</a></li>
                                     <li class="atcui-list-item"><a href="http://digitalsolutions.autotrader.com/advertising-solutions/">Autotrader Ad Specs</a></li>
                                     <li class="atcui-list-item"><a href="/legal/visitor-agreement.xhtml" onclick="return false"><span id="visitorLink" class="atcui-trigger atcui-icon-link atcui-icon-span" data-atcui-trigger-target="popupVisitor" data-atcui-trigger-event="click" data-atcui-trigger-method="open">Visitor Agreement</span></a></li>
                                     <li class="atcui-list-item"><a href="/privacy.jsp">Privacy Statement</a></li>
                                     <li class="atcui-list-item"><a href="/new_features.jsp">New Features on ATC</a></li>
-                                    <li class="atcui-list-item"><a href="http://digitalsolutionsautotrader.com/autotrader-offsite/">Autotrader Access</a></li>
+                                    <li class="atcui-list-item"><a href="http://digitalsolutionsautotrader.com/autotrader-offsite/">Autotrader Access</a></li>-->
                                 </ul>
                             </div>
                         </div>
